55440ed70a41b592f29bf15029f28af12bb7246f
drawitnow <sasha.romanuyk@gmail.com>
2022-06-01 00:08:00 +0200

    form test

--- send.php
+++ send.php
@@ -1,25 +1,23 @@
 <?php
-    $fio = $_POST['fio'];
-    $email = $_POST['email'];
-    $tel = $_POST['tel'];
-    $fio = htmlspecialchars($fio);
-    $email = htmlspecialchars($email);
-    $tel = htmlspecialchars($tel);
-    $fio = urldecode($fio);
-    $email = urldecode($email);
-    $tel = urldecode($tel);
-    $fio = trim($fio);
-    $email = trim($email);
-    $tel = trim($tel);
-    //echo $fio;
-    //echo "<br>";
-    //echo $email;
-    //echo "<br>";
-    //echo $tel;
-    if (mail("kovalenko.olexandra@gmail.com", 
-        "Заявка с сайта", "Ім'я:".$fio.". E-mail: ".$email"  ,
-        "From: kovalenko.olexandra@gmail.com \r\n"))
-     {     echo "сообщение успешно отправлено";
-    } else {
-        echo "при отправке сообщения возникли ошибки";
+$fio = $_POST['fio'];
+$email = $_POST['email'];
+$tel = $_POST['tel'];
+$fio = htmlspecialchars($fio);
+$email = htmlspecialchars($email);
+$tel = htmlspecialchars($tel);
+$fio = urldecode($fio);
+$email = urldecode($email);
+$tel = urldecode($tel);
+$fio = trim($fio);
+$email = trim($email);
+$tel = trim($tel);
+//echo $fio;
+//echo "<br>";
+//echo $email;
+//echo "<br>";
+//echo $tel;
+if (mail("kovalenko.olexandra@gmail.com", "Заявка с сайта", "ФИО:".$fio.". Телефон:".$tel.". E-mail: ".$email ,"From: kovalenko.olexandra@gmail.com \r\n"))
+ {     echo "сообщение успешно отправлено";
+} else {
+    echo "при отправке сообщения возникли ошибки";
 }?>
