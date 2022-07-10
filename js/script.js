120e357f1243ca2b5705998bbbb7f75e2141a065
itliza <elizaveta.romaniuk@gmail.com>
2022-07-11 00:10:26 +0300

    edits js

--- js/script.js
+++ js/script.js
@@ -20,14 +20,13 @@ const btnDonutAll = document.querySelectorAll(".js-btn__modal");
 
 btnDonutAll.forEach((btnDonut) => {
   btnDonut.addEventListener("click", () => {
+    document.body.style.overflow = "hidden";
     document.querySelector(".popup__none").style.display = "flex";
-    document.querySelector("body").classList.add("lock");
   });
 
   const btnClose = document.querySelector(".btn__close");
   btnClose.addEventListener("click", () => {
     document.querySelector(".popup__none").style.display = "none";
-    document.querySelector("body").classList.remove("lock");
     document.body.style.overflow = "auto";
   });
 });
@@ -45,25 +44,12 @@ function copyToClipboard(element) {
 const btnCopyAll = document.querySelectorAll(".btn__copy");
 btnCopyAll.forEach((btnCopy, btnIndex) => {
   btnCopy.addEventListener("click", () => {
+    document.querySelector(".pop__copy").style.display = "flex";
+
     function dissapearText() {
-      document.querySelector(".swal2-container").style.display = "none";
+      document.querySelector(".pop__copy").style.display = "none";
     }
 
     setTimeout(dissapearText, 800);
-
-    Swal.fire({
-      position: "center",
-      title: "Скопійовано",
-      background: "transparent",
-      color: "#EBE8E7",
-      showConfirmButton: false,
-      backdrop: `
-        rgba(0, 0, 0, 0.7)
-      `,
-    });
-
-    const titleAlert = document.querySelector(".swal2-title");
-    titleAlert.style.fontSize = "16px";
-    titleAlert.style.fontWeight = "400";
   });
 });
