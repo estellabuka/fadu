ec767fa776e7870886dc404be53be984772e910c
itliza <elizaveta.romaniuk@gmail.com>
2022-07-10 23:11:50 +0300

    edits smalll

--- js/slider.js
+++ js/slider.js
@@ -1,15 +1,12 @@
-$('.slider__box').slick({
+$(".slider__box").slick({
   dots: false,
   infinite: true,
   speed: 300,
   slidesToShow: 1,
   centerMode: true,
-  variableWidth: true
+  variableWidth: true,
 });
 
-//   console.dir(document.querySelectorAll('.slick-arrow'));
-document.querySelectorAll('.slick-arrow').forEach(btn => {
-    btn.textContent = '';
-})
-
-  
+document.querySelectorAll(".slick-arrow").forEach((btn) => {
+  btn.textContent = "";
+});
